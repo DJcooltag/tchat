296944d2d41b9693f3e0ed7f54d30db0c8f0255b
DJcooltag <155681631+DJcooltag@users.noreply.github.com>
2025-07-16 11:35:24 +0200

    Delete group.js

--- group.js
+++
@@ -1,8 +0,0 @@
-// group.js
-function createGroup() {
-  const nom = document.getElementById("group-name").value.trim();
-  const user = localStorage.getItem("github_user");
-  if (!nom || !user) return alert("Nom ou utilisateur manquant");
-
-  alert("Groupe '" + nom + "' créé par " + user);
-}
